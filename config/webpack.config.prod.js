a262a6707313d43b9fd4fe21b8a6979990bea0c6
chenshijie <cruoru@gmail.com>
2018-08-16 18:26:49 +0800

    add scss in build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -101,7 +101,9 @@ module.exports = {
       // To fix this, we prevent you from importing files out of src/ -- if you'd like to,
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
-      new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+
+      // close outside src
+      // new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
     ],
   },
   module: {
@@ -127,6 +129,10 @@ module.exports = {
           },
         ],
         include: paths.appSrc,
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         // "oneOf" will traverse all following loaders until one will
@@ -222,7 +228,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/,/\.scss$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
